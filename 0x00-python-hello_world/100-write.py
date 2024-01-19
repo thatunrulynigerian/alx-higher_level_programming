5bc9c0589228d2d73e874ccfdee4e6a94d4b4a25
Maria Abiazim <mukpere777@gmail.com>
2024-01-19 00:43:57 -0800

    python

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,3 @@
-97% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
 #!/usr/bin/python3
 import sys
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
