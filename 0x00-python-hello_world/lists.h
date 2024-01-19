cb5c697f5d3b66fb60e9486a81e679848e40db9c
Maria Abiazim <mukpere777@gmail.com>
2024-01-19 01:17:03 -0800

    python

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,4 +1,3 @@
-97% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
 #ifndef LISTS_H
 #define LISTS_H
 
